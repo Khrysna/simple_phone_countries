cff9e427df045a55adf23897547c211de17efa25
Khrysna <brownkrisna@gmail.com>
2026-01-13 05:20:20 +0800

    feat: add example

---
+++ example/lib/widgets/country_flag.dart
@@ -0,0 +1,25 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_phone_countries/simple_phone_countries.dart';
+
+class CountryFlag extends StatelessWidget {
+  const CountryFlag({super.key, required this.country, this.width = 32, this.height = 24});
+
+  final CountryCode country;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      country.flagAssetPath,
+      width: width,
+      height: height,
+      placeholderBuilder: (context) => Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(color: Colors.grey[300], borderRadius: BorderRadius.circular(2)),
+      ),
+    );
+  }
+}
---
+++ example/lib/widgets/country_list_tile.dart
@@ -0,0 +1,20 @@
+﻿import 'package:flutter/material.dart';
+import 'package:simple_phone_countries/simple_phone_countries.dart';
+
+import 'country_flag.dart';
+
+class CountryListTile extends StatelessWidget {
+  const CountryListTile(this.country, {super.key});
+
+  final CountryCode country;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CountryFlag(country: country),
+      title: Text(country.name),
+      subtitle: Text('${country.code} • ${country.dialCode}'),
+      onTap: () {},
+    );
+  }
+}
